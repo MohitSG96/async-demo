4a382f756035835ae7f60466cca518cf401fdf2e
Mohit <mohit.g.mg1996@gmail.com>
2019-03-24 20:57:46 +0530

    creating function to get user

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 console.log('before');
-setTimeout(() => {
-    console.log('Reading a user from database...');
-}, 2000);
+const user = getUser(1);
+console.log(user);
 console.log('after');
+
+function getUser(id) {
+    setTimeout(() => {
+        console.log('Reading a user from database...');
+        return {
+            id: id,
+            gitHubUser: 'Mohit'
+        };
+    }, 2000);
+
+}
