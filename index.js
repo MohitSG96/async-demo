478149251d194fb748ad325322d9cb79ad6c1eba
Mohit <mohit.g.mg1996@gmail.com>
2019-03-24 21:26:41 +0530

    creating callback hell intentionally

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 console.log('before');
-const user = getUser(1);
-console.log(user);
+getUser(1, (user) => {
+    getRepositories(user.gitHubUser, (repos) => {
+        getCommits(repo, (commits) => {
+            //Callback Hell or callback chrismas tree
+        });
+    });
+});
 console.log('after');
 
-function getUser(id) {
+function getUser(id, callback) {
     setTimeout(() => {
         console.log('Reading a user from database...');
-        return {
+        callback({
             id: id,
             gitHubUser: 'Mohit'
-        };
+        });
     }, 2000);
 
+}
+
+function getRepositories(user, callback) {
+    setTimeout(() => {
+        console.log('Reading user repository from github...');
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
 }
